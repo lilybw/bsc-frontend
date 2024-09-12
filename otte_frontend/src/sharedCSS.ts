449b5aaa6ec94ffbf6435e9c1c62cbba3a65f6be
lilybw <lilybwanscher@gmail.com>
2024-09-12 17:51:18 +0200

    dev mode reenabled

--- otte_frontend/src/sharedCSS.ts
+++ otte_frontend/src/sharedCSS.ts
@@ -1,19 +1,20 @@
 import { css } from '@emotion/css'
 
 export const SHARED_CSS_STR =`
-    body {
+body {
     margin: 0;
+    background-color: #000000;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
         'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
         sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    }
+}
 
-    code {
+code {
     font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
         monospace;
-    }
+}
 `
 
 export const SHARED_CSS = css`${SHARED_CSS_STR}`
