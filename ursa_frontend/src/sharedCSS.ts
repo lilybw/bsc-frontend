f19e46f7b8a004088f012c576da667b6fc2d424d
lilybw <lilybwanscher@gmail.com>
2024-11-11 18:56:28 +0100

    Refactored sharedCSS to use object styles

--- ursa_frontend/src/sharedCSS.ts
+++ ursa_frontend/src/sharedCSS.ts
@@ -1,74 +1,72 @@
 import { css } from '@emotion/css';
 import { TransformDTO } from './integrations/main_backend/mainBackendDTOs';
 
-const titleBase = css`
-    font-family: 'Orbitron', sans-serif;
-    font-weight: 700;
-    letter-spacing: 1rem;
-    text-transform: uppercase;
-    color: white;
-    font-size: 8rem;
-    text-shadow: 0.75rem 0.3rem 0.3rem rgba(255, 255, 255, 0.3);
-    filter: drop-shadow(-0.1rem -0.2rem 0.2rem rgba(255, 255, 255, 0.5));
-`
+const titleBase = css({
+    fontFamily: "'Orbitron', sans-serif",
+    fontWeight: 700,
+    letterSpacing: "1rem",
+    textTransform: "uppercase",
+    color: "white",
+    fontSize: "8rem",
+    textShadow: "0.75rem 0.3rem 0.3rem rgba(255, 255, 255, 0.3)",
+    filter: "drop-shadow(-0.1rem -0.2rem 0.2rem rgba(255, 255, 255, 0.5))",
+})
 
-const subTitleBase = css`
-    text-align: center;
-    font-family: 'Orbitron', sans-serif;
+const subTitleBase = css({
+    textAlign: "center",
+    fontFamily: "'Orbitron', sans-serif",
 
-    font-weight: 700;
-    letter-spacing: 0;
-    color: hsla(0, 0%, 100%, 0.7);
+    fontWeight: 700,
+    letterSpacing: 0,
+    color: "hsla(0, 0%, 100%, 0.7)",
 
-    font-size: 2rem;
-    text-shadow: 0.15rem 0.15rem 0.3rem rgba(255, 255, 255, 0.3);
-    filter: drop-shadow(-0.1rem -0.2rem 0.2rem rgba(255, 255, 255, 0.5));
-`
+    fontSize: "2rem",
+    textShadow: "0.15rem 0.15rem 0.3rem rgba(255, 255, 255, 0.3)",
+    filter: "drop-shadow(-0.1rem -0.2rem 0.2rem rgba(255, 255, 255, 0.5))",
+})
 
 /**
  * Small style fragments
  */
 export const Styles = {
-    NO_OVERFLOW: css`
-        overflow: hidden !important;
-    `,
-    NO_SHOW: css`
-        display: none !important;
-    `,
+    NO_OVERFLOW: css({
+        overflow: "hidden",
+    }),
+    NO_SHOW: css({
+        display: "none",
+    }),
     OVERLAY: {
-        CENTERED_QUARTER: css`
-            position: absolute;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: space-evenly;
+        CENTERED_QUARTER: css({
+            position: "absolute",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "space-evenly",
 
-            top: 50%;
-            left: 50%;
-            width: 50%;
-            height: 50%;
-            transform: translate(-50%, -50%);
-        `
+            top: "50%",
+            left: "50%",
+            width: "50%",
+            height: "50%",
+            transform: "translate(-50%, -50%)",
+        }),
     },
     MINIGAME: {
-        TITLE: css`
-            ${titleBase}
-            font-size: 3rem;
-            color: black;
-            text-shadow: 0 0 0.5rem rgba(60, 140, 255, 1);
-        `,
-        DIFFICULTY_NAME: css`
-            ${subTitleBase}
-            text-shadow: none;
-            color: hsl(30, 80%, 50%);
-            text-transform: uppercase;
-        `
+        TITLE: css([titleBase, {  
+            fontSize: "3rem",
+            color: "black",
+            textShadow: "0 0 0.5rem rgba(60, 140, 255, 1)",
+        }]),
+        DIFFICULTY_NAME: css([subTitleBase, {
+            textShadow: "none",
+            color: "hsl(30, 80%, 50%)",
+            textTransform: "uppercase",
+        }])
     },
-    TRANSFORM_CENTER: css`
-        position: absolute;
-        left: 50%;
-        transform: translateX(-50%);
-    `,
+    TRANSFORM_CENTER: css({
+        position: "absolute",
+        left: "50%",
+        transform: "translateX(-50%)",
+    }),
     TITLE: titleBase,
     SUB_TITLE: subTitleBase,
     MENU_INPUT: css`
@@ -101,16 +99,16 @@ export const Styles = {
             margin: 0;
         }
     `,
-    CROSS_HATCH_GRADIENT: css`
-        background-image: repeating-linear-gradient(
+    CROSS_HATCH_GRADIENT: css({
+        backgroundImage: `repeating-linear-gradient(
             45deg,
             transparent,
             transparent 10px,
             rgba(128, 128, 128, 0.5) 10px,
             rgba(128, 128, 128, 0.5) 20px
-        );
-        background-size: 28.28px 28.28px;
-    `,
+        )`,
+        backgroundSize: "28.28px 28.28px",
+    }),
     transformToCSSVariables: (transform?: TransformDTO) => css`
         --transform-x: ${transform ? transform.xOffset : 0}px;
         --transform-y: ${transform ? transform.yOffset : 0}px;
@@ -128,25 +126,25 @@ export const Styles = {
      * * --transform-xScale: x scale, any float
      * * --transform-yScale: y scale, any float
      */
-    TRANSFORM_APPLICATOR: css`
-        position: absolute;
-        left: var(--transform-x);
-        top: var(--transform-y);
-        z-index: var(--transform-index);
-        transform: scale(var(--transform-xScale), var(--transform-yScale)) translate(-50%, -50%);
-    `,
-    FANCY_BORDER: css`
-        border-radius: 5vh;
-        border: 0.25rem solid white;
-        border-left: 0px;
-        border-right: 0px;
+    TRANSFORM_APPLICATOR: css({
+        position: "absolute",
+        left: "var(--transform-x)",
+        top: "var(--transform-y)",
+        zIndex: "var(--transform-index)",
+        transform: "scale(var(--transform-xScale), var(--transform-yScale)) translate(-50%, -50%)",
+    }),
+    FANCY_BORDER: css({
+        borderRadius: "5vh",
+        border: "0.25rem solid white",
+        borderLeft: "0px",
+        borderRight: "0px",
 
-        backdrop-filter: blur(0.5rem);
-        -webkit-backdrop-filter: blur(0.5rem); // For Safari support
-        box-shadow:
+        backdropFilter: "blur(0.5rem)",
+        boxShadow: `
             0 0 1rem rgba(255, 255, 255, 0.2) inset,
-            0 0 1rem black;
-    `,
+            0 0 1rem black
+            `,
+    }),
     ANIM_FADE_OUT: (seconds: number) => css`
         opacity: 1;
         animation: fadeOut ${seconds}s linear;
@@ -160,14 +158,14 @@ export const Styles = {
         }
     `,
     GLASS: {
-        FAINT_BACKGROUND: css`
-            background-color: rgba(0, 0, 0, 0.3);
-            backdrop-filter: blur(0.3rem);
-        `,
-        BACKGROUND: css`
-            background-color: rgba(0, 0, 0, 0.5);
-            backdrop-filter: blur(0.5rem);
-        `,
+        FAINT_BACKGROUND: css({
+            backgroundColor: "rgba(0, 0, 0, 0.3)",
+            backdropFilter: "blur(0.3rem)",
+        }),
+        BACKGROUND: css({
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            backdropFilter: "blur(0.5rem)",
+        }),
     },
 };
 export const BigButtonStyle = css`
