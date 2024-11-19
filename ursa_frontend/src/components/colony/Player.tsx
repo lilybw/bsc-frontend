1c90373b0b822169428f8cc2b33ea6536c4398fc
Klaes Sørensen <Klaesdrescher@gmail.com>
2024-11-19 03:10:12 +0100

    cleanup and outline

--- ursa_frontend/src/components/colony/Player.tsx
+++ ursa_frontend/src/components/colony/Player.tsx
@@ -5,6 +5,7 @@ import { Styles } from '../../styles/sharedCSS';
 import { ClientDTO } from '../../integrations/multiplayer_backend/multiplayerDTO';
 import { WrappedSignal } from '../../ts/wrappedSignal';
 import { IBackendBased, IStyleOverwritable } from '../../ts/types';
+import { ObjectURL } from '../../integrations/main_backend/objectUrlCache';
 import NTAwait from '../util/NoThrowAwait';
 import GraphicalAsset from '../base/GraphicalAsset';
 
@@ -21,8 +22,52 @@ interface PlayerProps extends IStyleOverwritable, IBackendBased {
 const UNIT_TRANSFORM: TransformDTO = { xOffset: 0, yOffset: 0, zIndex: 0, xScale: 0, yScale: 1 };
 const MAX_HUE = 300;
 
+const getHueRotatedColor = async (url: ObjectURL, hue: number): Promise<string> => {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return '#ffffff';
+
+    const img = new Image();
+    img.src = url;
+
+    const promise = new Promise<string>((resolve, reject) => {
+        img.onload = () => {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.filter = `hue-rotate(${hue}deg)`;
+            ctx.drawImage(img, 0, 0);
+
+            const avgCanvas = document.createElement('canvas');
+            const avgCtx = avgCanvas.getContext('2d');
+            if (!avgCtx) {
+                canvas.remove();
+                resolve('#ffffff');
+                return;
+            }
+
+            avgCanvas.width = 1;
+            avgCanvas.height = 1;
+            avgCtx.drawImage(canvas, 0, 0, img.width, img.height, 0, 0, 1, 1);
+
+            const [r, g, b] = avgCtx.getImageData(0, 0, 1, 1).data;
+            const color = `rgb(${r}, ${g}, ${b})`;
+
+            canvas.remove();
+            avgCanvas.remove();
+            resolve(color);
+        };
+        img.onerror = () => {
+            canvas.remove();
+            resolve('#ffffff');
+        };
+    });
+
+    return await promise;
+};
+
 const Player: Component<PlayerProps> = (props) => {
     const [currentTransform, setCurrentTransform] = createSignal<TransformDTO>(UNIT_TRANSFORM);
+    const [averageColor, setAverageColor] = createSignal('#ffffff');
 
     createEffect(() => {
         const currentLoc = props.client.state.lastKnownPosition;
@@ -43,40 +88,50 @@ const Player: Component<PlayerProps> = (props) => {
         return segmentSize * (props.clientIndex - 1);
     });
 
-    const computedContainerStyles = createMemo(
-        () => css`
-            --GAS: ${props.GAS()};
-            ${playerContainer}
-            ${Styles.POSITION.transformToCSSVariables(currentTransform())}
-            ${Styles.POSITION.TRANSFORM_APPLICATOR}
-            filter: hue-rotate(${computedHue()}deg);
-            ${props.styleOverwrite}
-        `,
-    );
-
     return (
-        <div class={computedContainerStyles()} id={'player-' + props.client.IGN}>
-            <div class={imageContainer}>
+        <div
+            class={css`
+                ${containerBase}
+                ${Styles.POSITION.transformToCSSVariables(currentTransform())}
+                ${Styles.POSITION.TRANSFORM_APPLICATOR}
+                ${props.styleOverwrite}
+            `}
+            id={'player-' + props.client.IGN}
+        >
+            <div class={css`filter: hue-rotate(${computedHue()}deg);`}>
                 <NTAwait func={() => props.backend.assets.getMetadata(4002)}>
-                    {(asset) => (
-                        <GraphicalAsset
-                            metadata={asset}
-                            backend={props.backend}
-                            transform={{
-                                xOffset: 0,
-                                yOffset: 0,
-                                zIndex: 0,
-                                xScale: props.GAS(),
-                                yScale: props.GAS()
-                            }}
-                            styleOverwrite={css`
-                                position: absolute;
-                                top: 50%;
-                                left: 50%;
-                                transform: translate(-50%, -50%);
-                            `}
-                        />
-                    )}
+                    {(asset) => {
+                        if (asset.LODs[0]) {
+                            props.backend.objectUrlCache.getByLODID(asset.LODs[0].id)
+                                .then(urlResult => {
+                                    if (!urlResult.err && urlResult.res) {
+                                        getHueRotatedColor(urlResult.res, computedHue())
+                                            .then(color => setAverageColor(color));
+                                    }
+                                });
+                        }
+
+                        return (
+                            <GraphicalAsset
+                                metadata={asset}
+                                backend={props.backend}
+                                transform={{
+                                    xOffset: 0,
+                                    yOffset: 0,
+                                    zIndex: 0,
+                                    xScale: props.GAS(),
+                                    yScale: props.GAS()
+                                }}
+                                styleOverwrite={css`
+                                    position: absolute;
+                                    top: 50%;
+                                    left: 50%;
+                                    transform: translate(-50%, -50%);
+                                    filter: drop-shadow(0 0 8px ${averageColor()});
+                                `}
+                            />
+                        );
+                    }}
                 </NTAwait>
             </div>
             {props.showNamePlate && (
@@ -88,43 +143,23 @@ const Player: Component<PlayerProps> = (props) => {
     );
 };
 
-export default Player;
-
-const imageContainer = css`
-    position: relative;
-    width: 100%;
-    height: 100%;
-`;
-
-const namePlateStyle = css`
-    position: absolute;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 201;
-    bottom: -7vh;
-    left: 50%;
-    height: 5vh;
-    width: max-content;
-    padding: 0.5rem 1rem;
-    transform: translateX(-50%);
-    color: white;
-    font-size: 1.5rem;
-    text-align: center;
-    text-shadow: 0 0 5px black;
-    ${Styles.GLASS.FAINT_BACKGROUND}
-`;
-
-const playerContainer = css`
+const containerBase = css`
+    --GAS: 1;
     z-index: 200;
     width: calc(50px * var(--GAS));
     height: calc(50px * var(--GAS));
     position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    transition:
-        top 0.5s ease-in-out,
-        left 0.5s ease-in-out,
-        filter 0.3s ease-in-out;
 `;
+
+const namePlateStyle = css`
+    position: absolute;
+    bottom: -7vh;
+    left: 50%;
+    transform: translateX(-50%);
+    color: white;
+    font-size: 1.5rem;
+    text-shadow: 0 0 5px black;
+    ${Styles.GLASS.FAINT_BACKGROUND}
+`;
+
+export default Player;
